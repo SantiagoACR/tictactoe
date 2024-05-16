439a39551e7653ec95a6400842f7879e1a8b1c78
SantiagoACR <santiagoctpro@gmail.com>
2024-05-16 15:13:51 -0500

    document formated

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-const spaces = document.querySelectorAll(".space")
+const spaces = document.querySelectorAll(".space");
 
-let player = "X"
+let player = "X";
 
-const printplayer = (nSpace)=> {
-    if(isEmpty(nSpace)){
-        spaces[nSpace].innerHTML = player;
-        player = player == "X" ? "O" : "X";
-    }else{
-        alert("Espacio Ocupado")
-    }
-}
+let xowinner = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+];
+
+const printplayer = (nSpace) => {
+  if (isEmpty(nSpace)) {
+    spaces[nSpace].innerHTML = player;
+    player = player == "X" ? "O" : "X";
+  } else {
+    alert("Espacio Ocupado");
+  }
+};
 
 const isEmpty = (nS) => {
-    if(spaces[nS].innerHTML == ""){
-        return true
-    }else{
-        return false
-    }
-}
+  if (spaces[nS].innerHTML == "") {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-spaces.forEach(space => {
-    space.addEventListener("click", () => { 
-        printplayer(parseInt(space.id)-1)
-    })
+spaces.forEach((space) => {
+  space.addEventListener("click", () => {
+    printplayer(parseInt(space.id) - 1);
+  });
 });
